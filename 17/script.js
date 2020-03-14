0307dcc248755fa0d089168d3a92e8ce99d86a09
Tej <tejpartapsingh91@gmail.com>
2020-03-14 09:32:34 -0400

    fix bugs

--- 17/script.js
+++ 17/script.js
@@ -1,5 +1,4 @@
 const app = document.getElementById("app");
-const blankCart = document.getElementById("blankCart");
 const search = document.querySelector("#search");
 
 async function getData() {
@@ -12,68 +11,95 @@ async function getData() {
   }
 }
 
+// LSI = Local Storage Item
+function getLSI(item) {
+  return JSON.parse(localStorage.getItem(item));
+}
+function setLSI(key, item) {
+  return localStorage.setItem(key, JSON.stringify(item));
+}
+
 function handleDelete(e, index) {
   e.preventDefault();
-  const bucket = localStorage.getItem("item")
-    ? JSON.parse(localStorage.getItem("item"))
-    : [];
+  const bucket = getLSI("item") ? getLSI("item") : [];
   bucket.length === 0 && false;
   bucket.splice(index, 1);
-  localStorage.setItem("item", JSON.stringify(bucket));
+  setLSI("item", bucket);
   showData();
 }
 
 function showData() {
-  const bucket = localStorage.getItem("item")
-    ? JSON.parse(localStorage.getItem("item"))
-    : [];
+  let html = ``;
+  const bucket = getLSI("item") ? getLSI("item") : [];
   bucket && bucket.length !== 0
     ? (app.classList.add(`d-block`), blankCart.classList.add(`d-none`))
     : (app.classList.remove(`d-none`), blankCart.classList.remove(`d-none`));
-  let html = ``;
-  bucket.forEach(function(ele) {
+  bucket.forEach(function(element) {
     html += `
       <div class="col col-12 col-sm-6 col-md-4">
         <div class="card overflow-hidden mb-3">
           <div class="d-flex justify-content-center bg-dark">
-            <img class="img-fluid" src=${ele.image} class="card-img-top" alt="Image of an item" />
+            <img class="img-fluid" src=${element.image} class="card-img-top" alt="Image of an item" />
           </div>
           <div class="card-body">
-            <h5 class="card-title">${ele.name}</h5>
+            <h5 class="card-title">${element.name}</h5>
             <p class="card-text">
-              ${ele.price}
+              ${element.price}
             </p>
-            <a href="#" class="btn btn-danger" id="remove" data-id=${ele.id}>- Remove</a>
+            <a href="#" class="btn btn-danger" id="remove" data-id=${element.id}>- Remove</a>
           </div>
         </div>
       </div>`;
   });
-  document.getElementById("app").querySelector(".row").innerHTML = html;
-  document.querySelectorAll("#remove").forEach(function(ele, index) {
-    ele.addEventListener("click", function(e) {
+  app.querySelector(".row").innerHTML = html;
+  const remove = document.querySelectorAll("#remove");
+  remove.forEach(function(element, index) {
+    element.addEventListener("click", function(e) {
       handleDelete(e, index);
     });
   });
 }
 
 function handleData(data) {
-  const cart = localStorage.getItem("item")
-    ? JSON.parse(localStorage.getItem("item"))
-    : [];
   document.forms[0].addEventListener("submit", function(e) {
     e.preventDefault();
     const barCode = e.target[0].value;
     if (isNaN(barCode.trim()) || barCode.trim() === "") {
+      alert("Please enter item's bar code or ask nearby cashier for help.");
       return;
     }
-    const purchasedItem = data.forEach(function(currentItem) {
-      currentItem.id === parseInt(barCode) ? cart.push(currentItem) : false;
+    const matchingItems = getLSI("item") ? getLSI("item") : [];
+    Array.from(data).forEach(function(currentItem) {
+      if (parseInt(currentItem.id) === parseInt(barCode)) {
+        matchingItems.push(currentItem);
+      }
     });
-    localStorage.setItem("item", JSON.stringify(cart));
+    setLSI("item", matchingItems);
     showData();
     e.target.reset();
   });
 }
+
+search.addEventListener("input", function(e) {
+  e.preventDefault();
+  const searchNotFound = document.getElementById("searchNotFound");
+  const cards = document.querySelectorAll("#app .card");
+  cards.length === 0 && false;
+  cards.forEach(currentItem => {
+    if (
+      currentItem
+        .querySelector(".card-title")
+        .textContent.toLowerCase()
+        .includes(e.target.value.trim().toLowerCase())
+    ) {
+      currentItem.parentElement.style.display = `block`;
+      searchNotFound.classList.add(`d-none`);
+    } else {
+      currentItem.parentElement.style.display = `none`;
+      searchNotFound.classList.remove(`d-none`);
+    }
+  });
+});
 
 getData()
   .then(function(data) {
@@ -84,25 +110,3 @@ getData()
   });
 
 showData();
-
-search.addEventListener("change", function(e) {
-  e.preventDefault();
-  const bucket = document.querySelectorAll(`#app .card`);
-  bucket.length === 0 && false;
-  bucket.forEach(currentItem => {
-    if (
-      currentItem
-        .querySelector(".card-title")
-        .textContent.toLowerCase()
-        .includes(e.target.value.trim().toLowerCase())
-    ) {
-      currentItem.parentElement.style.display = `block`;
-      blankCart.classList.add(`d-none`);
-      blankCart.classList.remove(`d-block`);
-    } else {
-      currentItem.parentElement.style.display = `none`;
-      blankCart.classList.remove(`d-none`);
-      blankCart.classList.add(`d-block`);
-    }
-  });
-});
