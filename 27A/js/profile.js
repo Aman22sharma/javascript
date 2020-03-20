2d7e3fdda5da8958dfdabf88436d1f12df9ff007
Tej <tejpartapsingh91@gmail.com>
2020-03-20 14:56:21 -0400

    revise

--- 27A/js/profile.js
+++ 27A/js/profile.js
@@ -1,4 +1,5 @@
 const status = document.getElementById("status");
+const content = document.getElementById("content");
 const signOutButton = document.getElementById("signOutButton");
 
 const auth = firebase.auth();
@@ -18,10 +19,11 @@ signOutButton.addEventListener("click", e => {
 
 auth.onAuthStateChanged(user => {
   if (user) {
+    content.innerHTML = `<div class="user"><h1 class="user__name">Welcome ${user.displayName}</h1><div class="user__thumbnail"><img src=${user.photoURL}></div>`;
     if (user.emailVerified) {
-      status.innerHTML = `<h1>Welcome ${user.displayName}</h1><p><img src=${user.photoURL}></p><p>Your email <strong>${user.email}</strong> has been verified.</p>`;
+      status.innerHTML = `<p class="status__text status__text--success">Your account has been verified.</p>`;
     } else {
-      status.innerHTML = `<h1>Welcome ${user.displayName}</><p><img src=${user.photoURL}></p><p>Your email <strong>${user.email}</strong> needs to be verified. Please check your inbox.</p>`;
+      status.innerHTML = `<p class="status__text status__text--warning">Your account needs to be verified. Please check your inbox.</p></div>`;
     }
   } else {
     window.location.assign("./index.html");
