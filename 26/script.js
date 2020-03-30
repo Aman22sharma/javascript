4c1f47fe93bb1f1a1a9928b6934319174bd35565
Tej <tejpartapsingh91@gmail.com>
2020-03-30 13:39:50 -0400

    revise

--- 26/script.js
+++ 26/script.js
@@ -3,44 +3,75 @@ const app = document.getElementById(`app`);
 const database = firebase.database();
 const rootRef = database.ref("/messages");
 
-document.forms[0].addEventListener("submit", e => {
+const makeCall = async url => {
+  try {
+    const req = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const status = req.status;
+    return status;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+document.getElementById("form").addEventListener("submit", e => {
   e.preventDefault();
   const inputValue = e.target[0].value;
-  if (!inputValue || inputValue.trim() === "") {
-    alert(`Please enter a message to send!`);
-    return;
-  }
-  const autoId = rootRef.push().key;
-  rootRef
-    .child(autoId)
-    .set({
-      message: inputValue
-      // likes: 0
+  makeCall(inputValue)
+    .then(status => {
+      if (status === 200) {
+        const autoId = rootRef.push().key;
+        rootRef
+          .child(autoId)
+          .set({
+            message: inputValue
+          })
+          .then(() => {
+            let el = document.querySelector(".modal");
+            el.querySelector("h4").textContent = `Thank You!`;
+            el.querySelector(
+              "p"
+            ).textContent = `Your link has been successfully added!`;
+            let instance = M.Modal.init(el);
+            instance.open();
+            document.getElementById("form").reset();
+          })
+          .catch(error => console.log(error));
+      }
+      if (status !== 200) {
+        let el = document.querySelector(".modal");
+        el.querySelector("h4").textContent = `Error!`;
+        el.querySelector(
+          "p"
+        ).textContent = `Please verify all information before proceeding!`;
+        let instance = M.Modal.init(el);
+        instance.open();
+        return;
+        return;
+      }
     })
-    .then(() => {
-      alert(`Message added!`);
-      document.forms[0].reset();
-    })
-    .catch(error => console.log(error));
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 const handleData = data => {
   data.forEach(i => {
-    console.log(i);
-    let card = document.createElement("div");
-    let like = document.createElement("button");
-    let para = document.createElement("p");
-    para.textContent = `${i.val()}`;
-    // para.textContent = `${i.val()}, Likes: ${likes}`;
-    like.textContent = "Like";
-    card.appendChild(para);
-    card.appendChild(like);
-    app.appendChild(card);
+    let link = document.createElement("a");
+    link.classList.add("collection-item");
+    link.textContent = `${i.val()}`;
+    link.setAttribute("href", `${i.val()}`);
+    link.setAttribute("target", `_blank`);
+    app.appendChild(link);
   });
 };
 
-const handleLike = () => {
-
-}
+rootRef.on("value", snapshot => {
+  if (snapshot.val() == null) {
+    app.innerHTML = `<a href='#' class='collection-item none'>No links available yet.</a>`;
+  } else {
+    document.querySelector(".none").style.display = `none`;
+  }
+});
 
 rootRef.on("child_added", handleData);
+rootRef.on("child_changed", handleData);
